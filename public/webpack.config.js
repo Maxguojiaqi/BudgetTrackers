762ec1ba3939b91ce26a5b14dd7680285a9bbd6b
Max <maxguojiaqi@gmail.com>
2020-06-16 09:32:12 -0400

    add service worker files and webpack

---
+++ public/webpack.config.js
@@ -0,0 +1,45 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./index.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      name: "BudgetTracker",
+      short_name: "BT",
+      description: "",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("icons/icon-512x512.png"),
+        sizes: [192,512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ]
+};
+
+module.exports = config;
